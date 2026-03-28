fcc64d823f60fdd4085a94e868b4cb1ed2ef934d
matheusvirissimo <matheus.virissimo@unesp.br>
2026-03-28 02:02:24 -0300

    feat: (1) estatística descritiva

---
+++ aula-2/practice_2_2.R
@@ -0,0 +1,65 @@
+# install.packages("NHANES")
+
+# Carregar dados
+# %%
+library(NHANES)
+data("NHANES")
+dados <- NHANES
+
+# ===========================================
+# 1 selecionar um subconjunto do banco de dados do NHANES
+names(dados)
+colnames(dados)
+# selecionarei Age, Weight, Height, BMI
+df_subset <- dados[, c("Age", "Weight", "Height", "BMI")]
+teste <- c("Age", "Weight", "Height", "BMI")
+
+library(tidyverse)
+glimpse(df_subset)
+
+# %%
+# tendência central 
+# o sapply é parecidissimo com o map() do py
+media <- sapply(df_subset, mean, na.rm = TRUE) # média
+cat("======= Média ======\n",
+    "Idade: ", media["Age"], "\n", # nolint
+    "Peso: ", media["Weight"], "\n",
+    "Altura: ", media["Height"], "\n",
+    "IMC: ", media["BMI"], "\n"
+)
+mediana <- sapply(df_subset, median, na.rm = TRUE) # mediana
+cat("======= Mediana ======\n",
+    "Idade: ", mediana["Age"], "\n", # nolint
+    "Peso: ", mediana["Weight"], "\n",
+    "Altura: ", mediana["Height"], "\n",
+    "IMC: ", mediana["BMI"], "\n"
+)
+
+# medidas de dispersão
+variancia <- sapply(df_subset, var, na.rm = TRUE) # VARIANCIA
+cat("======= Variância ======\n",
+    "Idade: ", variancia["Age"], "\n", # nolint
+    "Peso: ", variancia["Weight"], "\n",
+    "Altura: ", variancia["Height"], "\n",
+    "IMC: ", variancia["BMI"], "\n"
+)
+
+desvio_padrao <- sapply(
+  X = df_subset, # vetor
+  FUN = sd, # função a ser aplicado
+  na.rm = TRUE
+) #desvio standar
+cat("======= desvio_padrao ======\n",
+    "Idade: ", desvio_padrao["Age"], "\n", # nolint
+    "Peso: ", desvio_padrao["Weight"], "\n",
+    "Altura: ", desvio_padrao["Height"], "\n",
+    "IMC: ", desvio_padrao["BMI"], "\n"
+)
+
+# minimo, máximo e intervalos
+cat("======= Minimo e Máximo ======\n",
+    "Idade mínima: ", min(df_subset$Age), " e máxima: ", max(df_subset$Age), "\n", # nolint
+    "Peso mínima: ", min(df_subset$Height, na.rm = TRUE), " e máxima: ", max(df_subset$Height, na.rm = TRUE), "\n", # nolint
+    "Altura mínima: ", min(df_subset$Weight, na.rm = TRUE), " e máxima: ", max(df_subset$Weight, na.rm = TRUE), "\n", # nolint
+    "IMC mínima: ", min(df_subset$BMI, na.rm = TRUE), " e máxima: ", max(df_subset$BMI, na.rm = TRUE), "\n" # nolint
+)
